eea6a2a966d017ad81eff761afd25728c3c43f10
ifkusyoba <ifkusyobaa@gmail.com>
2023-08-21 09:59:54 +0700

    Fix problem with .env

--- config/DbConfig.js
+++ config/DbConfig.js
@@ -1,8 +1,9 @@
+const dotenv = require('dotenv').config();
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'api_mahasiswa',
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASS,
+    DB: process.env.DB_NAME,
     dialect: 'mysql',
     pool: {
         max: 5,
